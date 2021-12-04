91b16efdefe0fbd0bedc362fbe8f52e63fdb7550
Terwer <cbgtyw@gmail.com>
2021-12-05 01:02:39 +0800

    MyBatis的基本使用

---
+++ src/test/java/com/test/test/MyBatisTest.java
@@ -0,0 +1,30 @@
+package com.test.test;
+
+import com.test.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MyBatisTest {
+    @Test
+    public void test() throws Exception {
+InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+
+SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+SqlSession sqlSession = sqlSessionFactory.openSession();
+
+List<User> users = sqlSession.selectList("user.findAll");
+
+for (User user : users) {
+    System.out.println(user);
+}
+
+sqlSession.close();
+    }
+}
